cc4ef6d3c6542a5161c852ad80aa27aeac3ed644
Adam LeBlacn <adam@Adams-MacBook-Air.local>
2018-11-04 02:04:25 -0400

    added script to install homebrew

---
+++ homebrew/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if test ! $(which brew)
+then
+    if test "$(uname)" = "Darwin"
+    then
+        echo "Installing homebrew"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+fi
